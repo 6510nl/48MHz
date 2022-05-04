10c805130939458a0a089f8d77d81c4a1f6ec811
Berry <berry@berrydejager.com>
2022-05-05 01:02:15 +0200

    Update compile.sh
    loop

--- toolchain/compile.sh
+++ toolchain/compile.sh
@@ -1,4 +1,7 @@
-!#!/usr/bin/env bash
-pgrep u64view || ./u64view/u64view -u u64.lab.home -z 3 -t </dev/null &>/dev/null &
+clear
+pgrep u64view &>/dev/null || ./u64view/u64view -u u64.lab.home -z 3 -t </dev/null &>/dev/null &
 java -jar ./KickAssembler/KickAss.jar ../source/main.asm -showmem
 perl ./1541ultimate2-tools/1541u2.pl u64.lab.home -c run:../source/main.prg
+echo -e
+read -p "Press enter to re-try"
+./compile.sh
